a4f88506a3b54bfd3328008a68f660ed13efc218
Kareem Brathwaite-Henry <kareembrathwaite-henry@Kareems-MacBook-Pro.local>
2022-09-07 09:47:59 -0400

    stlyed navbar and added test pages to test navigation

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,38 +1,40 @@
-import React, {useState} from 'react'
+import React from 'react'
+import { Link } from "gatsby"
 import './navbar.scss'
 
 const Navbar = () => {
 
-    //State that toggles mobile menu
-    const [toggleMenu, setToggleMenu] = useState(false);
+    // Toggles mobile menu open and close
     const toggleHamburgerMenu = () => {
-        let navbar = document.querySelector('.navbar-menu');
+        let navbarMenu = document.querySelector('.navbar-menu');
         let hamburgerToggle = document.querySelector('.navbar-burger');
-        navbar.classList.toggle('is-active');
+        let navbar = document.querySelector('.navbar');
+        navbarMenu.classList.toggle('is-active');
         hamburgerToggle.classList.toggle('is-active');
+        navbar.classList.toggle('is-active');
     }
 
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
-                <a className="navbar-item" href="https://bulma.io">
-                    <h2>InnerCircle</h2>
-                </a>
+                <Link className="navbar-item" to="/">
+                    <h2 className='navbar__title'>InnerCircle</h2>
+                </Link>
 
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={toggleHamburgerMenu}>
+                <span role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" onClick={toggleHamburgerMenu}>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
-                </a>
+                </span>
             </div>
 
-            <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-menu">
                 <div className="navbar-end">
-                    <a className="navbar-item">Services</a>
-                    <a className="navbar-item">About</a>
-                    <a className="navbar-item">Blog</a>
-                    <a className="navbar-item">Language Picker</a>
-                    <a className="navbar-item">Contact</a>
+                    <Link to="/services/" className="navbar-item">Services</Link>
+                    <Link to="/about/" className="navbar-item">About</Link>
+                    <Link to="/blog/" className="navbar-item">Blog</Link>
+                    <Link className="navbar-item">Language Picker</Link>
+                    <Link to="/contact/" className="navbar-item navbar__contact-cta">Contact</Link>
                 </div>
             </div>
         </nav>
