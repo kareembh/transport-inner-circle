c44b524e66daf254133eb86bc8135d7207f010b8
Kareem Brathwaite-Henry <kareembrathwaite-henry@Kareems-MacBook-Pro.local>
2022-09-21 10:47:42 -0400

    added responsiveness to hero banner section

--- src/components/hero-banner/hero-banner.js
+++ src/components/hero-banner/hero-banner.js
@@ -3,23 +3,20 @@ import './hero-banner.scss'
 
 const HeroBanner = () => {
   return (
-    <div className='hero-banner'>
-        <div className='hero-banner__black'>
-            <div className='hero-banner__content'>
-                <h1 className='hero-banner__title'>Your All-In-One Freight Company</h1>
-                <p className='hero-banner__text'>
-                    Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. In nec arcu ullamcorper, bib
-                    endum libero et, commodo metus. Vesti
-                    bulum rutrum convallis mattis.
-                </p>
-            </div>
+    <section className="section hero-banner">
+        <div className='hero-banner__content'>
+            <h1 className='hero-banner__title'>Your All-In-One Freight Company</h1>
+            <p className='hero-banner__text'>
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. In nec arcu ullamcorper, bib
+                endum libero et, commodo metus. Vesti
+                bulum rutrum convallis mattis.
+            </p>
         </div>
-        <div className='hero-banner__white'></div>
-        <div className='hero-banner__image'></div>
-        {/* need to do this but with image */}
-        {/* <img className='hero-banner__image' src='./../../images/hero-banner-image.png' alt='' /> */}
-    </div>
+        <div className='hero-banner__image-container'>
+            <img className='hero-banner__image' src='../../images/hero-banner-image.png' alt='' />
+        </div>
+    </section>
   )
 }
 
